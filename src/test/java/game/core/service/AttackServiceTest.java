c0c17eb963032c4f307cf997339c5f792c2043a7
SeungChan <dlatmdcks60@gmail.com>
2025-11-13 22:32:51 +0900

    test(AttackService): 단위테스트 수정

--- src/test/java/game/core/service/AttackServiceTest.java
+++ src/test/java/game/core/service/AttackServiceTest.java
@@ -1,19 +1,34 @@
 package game.core.service;
 
+import game.core.board.Board;
 import game.core.board.Position;
 import game.core.entity.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class AttackServiceTest {
-    @Test void basic_attack_reduces_hp_and_kills() {
-        Unit a = new Unit("A", Role.KNIGHT, TeamSide.PLAYER, Stats.of(10, 5, 0,0,0), Position.of(0,0));
-        Unit b = new Unit("B", Role.KNIGHT, TeamSide.ENEMY,  Stats.of(6,  1, 0,0,0), Position.of(0,1));
 
-        var svc = new AttackService();
-        svc.basicAttack(a, b);
-        assertThat(b.stats().hp()).isEqualTo(1);
-        svc.basicAttack(a, b);
-        assertThat(b.isDead()).isTrue();
+    @Test
+    void basic_attack_reduces_hp() {
+        Board b = new Board(5,4);
+        AttackService atk = new AttackService();
+
+        Unit attacker = new Unit("A", Role.KNIGHT, TeamSide.PLAYER,
+                Stats.of(30, 10, 0, 0, 0), Position.of(2,2));
+        Unit defender = new Unit("B", Role.MAGE, TeamSide.ENEMY,
+                Stats.of(25, 3, 0, 0, 0), Position.of(2,3));
+
+        List<Unit> all = List.of(attacker, defender);
+
+        ContextUnitsHolder.open(all);
+
+        atk.basicAttack(attacker, defender);
+
+        ContextUnitsHolder.close();
+
+        assertThat(defender.stats().hp()).isEqualTo(25 - 10);
     }
 }
