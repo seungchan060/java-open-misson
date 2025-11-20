e81c82fc3739de58231220f9375172144473bfa2
SeungChan <dlatmdcks60@gmail.com>
2025-11-20 17:21:39 +0900

    init: setup project

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "game.app"
+version = "0.1.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("game.app.GameApplication")
+}
